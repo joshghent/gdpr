9d9c8998201bca47618da8e36e7d54ac895e3779
Josh Ghent <me@joshghent.com>
2017-11-10 20:50:54 +0000

    Added proxy

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        'Fideloper\Proxy\TrustedProxyServiceProvider',
 
         /*
          * Application Service Providers...
