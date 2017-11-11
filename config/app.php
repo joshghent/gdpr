03103ce2dbb33a2ed983e93976a9b11819f73e47
Josh Ghent <me@joshghent.com>
2017-11-11 01:31:50 +0000

    Added rollbar

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          * Package Service Providers...
          */
         'Fideloper\Proxy\TrustedProxyServiceProvider',
+        Rollbar\Laravel\RollbarServiceProvider::class,
 
         /*
          * Application Service Providers...
